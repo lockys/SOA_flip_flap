72ce7c3778d1f744e8d1979a9622923c29283ce8
Calvin Jeng <locky4567@gmail.com>
2015-10-07 17:14:15 +0000

    revise yml_buddy module

--- yaml_buddy.rb
+++ yaml_buddy.rb
@@ -1,5 +1,6 @@
 # Module that can be included (mixin) to create and parse TSV data
 module YamlBuddy
+  require 'yaml'
   # @data should be a data structure that stores information
   #  from TSV or Yaml files. For example, it could be an Array of Hashes.
   attr_accessor :data
