fc93fc3999f7f5c65dafdcf9c5533858680e335c
eduardo-salazar <esalazar922@gmail.com>
2015-10-08 00:53:09 +0800

    Created Yaml Buddy to eliminate methods from flipflap main class

---
+++ yaml_buddy.rb
@@ -0,0 +1,18 @@
+# Module that can be included (mixin) to create and parse TSV data
+module YamlBuddy
+  # @data should be a data structure that stores information
+  #  from TSV or Yaml files. For example, it could be an Array of Hashes.
+  attr_accessor :data
+
+  # Process yml string.
+  # Return: a array of hash.
+  def take_yaml(yml)
+    @data = YAML.load(yml)
+  end
+  
+  # converts @data into yml string
+  # Return: yml format string
+  def to_yaml
+    @data.to_yaml
+  end
+end
